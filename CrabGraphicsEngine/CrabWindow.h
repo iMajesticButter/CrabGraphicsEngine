c55f2372ed9b80100fdf469c6df3c2182b974b5b
iMajesticButter <bl.wolenetz@gmail.com>
2019-09-27 16:20:21 -0700

    yaaaay

--- CrabGraphicsEngine/CrabWindow.h
+++ CrabGraphicsEngine/CrabWindow.h
@@ -9,20 +9,31 @@ namespace CrabEngine {
 
         class Window {
         public:
-            Window(int width, int height, unsigned MSAA);
+            Window(unsigned width, unsigned height, bool fullscreen, bool borderless, bool vSync, unsigned MSAA);
             ~Window();
 
-            void updateWindowSize();
+            void updateFrameBufferSize();
 
-            int width();
-            int height();
+            int fbWidth();
+            int fbHeight();
 
             GLFWwindow* glfwWindow();
 
             bool shouldClose();
 
+            void setWidth(const unsigned &width);
+            void setHeight(const unsigned &height);
+            void setFullscreen(const bool &fullscreen);
+            void setBorderless(const bool &borderless);
+            void setVSync(const bool &vSync);
+            void setMSAA(const unsigned &MSAAlevel);
+
+            void initialize();
+
         private:
-            int m_width, m_height;
+            int m_fbWidth, m_fbHeight;
+            unsigned m_width, m_height, m_MSAA;
+            bool m_fullscreen, m_borderless, m_vSync;
 
             GLFWwindow* m_window;
 
--- CrabGraphicsEngine/Window.cpp
+++ CrabGraphicsEngine/Window.cpp
@@ -1,6 +1,11 @@
 #include "CrabWindow.h"
 #include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void glfwErrorCallback(int err, const char* description) {
+    std::cerr << "ERROR: " << description << std::endl;
+}
 
 namespace CrabEngine {
     namespace Graphics {
@@ -9,29 +14,69 @@ namespace CrabEngine {
         // Public Functions
         //-------------------
 
-        Window::Window(int width, int height, unsigned MSAA) {
+        Window::Window(unsigned width, unsigned height, bool fullscreen, bool borderless, bool vSync, unsigned MSAA) {
+            //set window settings
 
+            setWidth(width);
+            setHeight(height);
+            setFullscreen(fullscreen);
+            setBorderless(borderless);
+            setVSync(vSync);
+            setMSAA(MSAA);
+
+            //initialize glfw
+            glfwInit();
+            glfwSetErrorCallback(glfwErrorCallback);
+
+            //create window
+            initialize();
         }
         Window::~Window() {
-
+            glfwDestroyWindow(m_window);
+            glfwTerminate();
         }
 
-        void Window::updateWindowSize() {
-
+        void Window::updateFrameBufferSize() {
+            glfwGetFramebufferSize(m_window, &m_fbWidth, &m_fbHeight);
         }
 
-        int Window::width() {
-
+        int Window::fbWidth() {
+            return m_fbWidth;
         }
-        int Window::height() {
-
+        int Window::fbHeight() {
+            return m_fbHeight;
         }
 
         GLFWwindow* Window::glfwWindow() {
-
+            return m_window;
         }
 
         bool Window::shouldClose() {
+            return glfwWindowShouldClose(m_window);
+        }
+
+        void Window::setWidth(const unsigned &width) {
+            m_width = width;
+        }
+        void Window::setHeight(const unsigned &height) {
+            m_height = height;
+        }
+        void Window::setFullscreen(const bool &fullscreen) {
+            m_fullscreen = fullscreen;
+        }
+        void Window::setBorderless(const bool &borderless) {
+            m_borderless = borderless;
+        }
+        void Window::setVSync(const bool &vSync) {
+            m_vSync = vSync;
+        }
+        void Window::setMSAA(const unsigned &MSAAlevel) {
+            m_MSAA = MSAAlevel;
+        }
+
+        void Window::initialize() {
+
+            //TODO: create window using settings and replace old one (if any), than call glewInit()
 
         }
 
